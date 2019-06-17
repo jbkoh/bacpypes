2001e8600dd47c9aa52816486fe903998996a207
Jason Koh <bk7749@gmail.com>
2019-06-17 02:55:43 -0700

    add semantic query service

---
+++ py34/bacpypes/service/semantic.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from ..debugging import bacpypes_debugging, ModuleLogger
+from ..capability import Capability
+
+from ..pdu import GlobalBroadcast
+
+from ..apdu import WhoIsRequest, IAmRequest, IHaveRequest, SimpleAckPDU
+from ..errors import ExecutionError, InconsistentParameters, \
+    MissingRequiredParameter, ParameterOutOfRange
+from ..task import FunctionTask
+
+# some debugging
+_debug = 0
+_log = ModuleLogger(globals())
+
+#
+#   SemanticQuery SemanticAnswer Services
+#
+
+@bacpypes_debugging
+class SemanticQueryServices(WhoHasIHaveServices): #extending WhoHasIHave to reuse i_have service.
+
+    def __init__(self):
+        if _debug: SemanticQueryServices._debug("__init__")
+        Capability.__init__(self)
+
+    def do_SemanticQueryRequest(self, apdu):
+        """Respond to a Who-Has request."""
+        if _debug: SemanticQueryServices._debug("do_SemanticQueryRequest, %r", apdu)
+
+        # ignore this if there's no local device
+        if not self.localDevice:
+            if _debug: WhoIsIAmServices._debug("    - no local device")
+            return
+
+        # find the object
+        objs = []
+        if apdu.tags is not None:
+            # TODO: Implement this
+            pdb.set_trace()
+            raise NotImplementedError("who_has with tags")
+        else:
+            raise InconsistentParameters("object identifier or object name required")
+
+        # maybe we don't have it
+        if not objs:
+            return
+
+        # send out the response
+        for obj in objs:
+            self.i_have(obj, address=apdu.pduSource)
+
